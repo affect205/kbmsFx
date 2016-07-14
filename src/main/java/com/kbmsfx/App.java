3538d8f0acef1c83ecbc2399d01cafa90c108851
Alex <alexbalu-alpha7@mail.ru>
2016-07-14 18:57:15 +0300

    startup + gui test

--- src/main/java/com/kbmsfx/App.java
+++ src/main/java/com/kbmsfx/App.java
@@ -7,33 +7,41 @@ package com.kbmsfx;
  */
 import com.kbmsfx.entity.Category;
 import com.kbmsfx.entity.TItem;
+import com.kbmsfx.gui.component.DisplayPanel;
 import com.kbmsfx.utils.CacheData;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.geometry.Side;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.web.HTMLEditor;
 import javafx.stage.Stage;
 import org.jboss.weld.environment.se.Weld;
 import org.jboss.weld.environment.se.WeldContainer;
 
-import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Singleton
 public class App extends Application {
 
-    private static Stage currentStage;
-
     @Inject
     private CacheData dataProvider;
 
-    private static CacheData dataProvider2;
+    @Inject
+    private DisplayPanel displayPanel;
+
+    private static  WeldContainer container;
 
     private final String INITIAL_TEXT = "Lorem ipsum dolor sit "
             + "amet, consectetur adipiscing elit. Nam tortor felis, pulvinar "
@@ -48,22 +56,16 @@ public class App extends Application {
     public static void main(String[] args) {
         System.out.print("init CDI container...");
         Weld weld = new Weld();
-        WeldContainer container = weld.initialize();
-        container.instance().select(App.class).get();
+        container = weld.initialize();
+        System.out.print("launch javaFx...");
         Application.launch(App.class, args);
-    }
-
-    @PostConstruct
-    public void init() {
-        if (dataProvider != null) dataProvider2 = dataProvider;
     }
 
     @Override
     public void start(Stage stage) {
-        currentStage = stage;
-        currentStage.setTitle("HTML");
-        currentStage.setWidth(500);
-        currentStage.setHeight(500);
+        stage.setTitle("HTML");
+        stage.setWidth(500);
+        stage.setHeight(500);
         Scene scene = new Scene(new Group());
 
         BorderPane root = new BorderPane();
@@ -72,20 +74,66 @@ public class App extends Application {
         root.setTop(buildTop());
         scene.setRoot(root);
 
-        currentStage.setScene(scene);
-        currentStage.show();
+        stage.setScene(scene);
+        stage.show();
     }
 
-    protected Control buildCenter() {
+    protected Pane buildCenter() {
+
         final HTMLEditor htmlEditor = new HTMLEditor();
         htmlEditor.setPrefHeight(600);
         htmlEditor.setHtmlText(INITIAL_TEXT);
-        return htmlEditor;
+        TitledPane titledPane = new TitledPane("HTML editor", htmlEditor);
+
+
+        TitledPane t1 = new TitledPane("T1", new Button("B1"));
+        TitledPane t2 = new TitledPane("T2", new Button("B2"));
+        TitledPane t3 = new TitledPane("T3", new Button("B3"));
+        Accordion accordion = new Accordion();
+        accordion.getPanes().addAll(t1, t2, t3);
+        //accordion.setRotate(270);
+
+        MenuButton m = new MenuButton("Eats");
+        m.setPrefWidth(100);
+        m.setPopupSide(Side.LEFT);
+        m.getItems().addAll(new MenuItem("Burger"), new MenuItem("Hot Dog"));
+
+        MenuButton m2 = new MenuButton("Drinks");
+        m2.setPrefWidth(100);
+        m2.setPopupSide(Side.LEFT);
+        m2.getItems().addAll(new MenuItem("Juice"), new MenuItem("Milk"));
+
+        MenuButton m3 = new MenuButton("Eats");
+        m3.setPrefWidth(100);
+        m3.setPopupSide(Side.BOTTOM);
+        m3.getItems().addAll(new MenuItem("Burger"), new MenuItem("Hot Dog"));
+
+        MenuButton m4 = new MenuButton("Drinks");
+        m4.setPrefWidth(100);
+        m4.setPopupSide(Side.BOTTOM);
+        m4.getItems().addAll(new MenuItem("Juice"), new MenuItem("Milk"));
+
+        VBox right = new VBox();
+        right.getChildren().addAll(m, m2);
+
+        HBox top = new HBox();
+        top.getChildren().addAll(m3, m4);
+
+
+        BorderPane center = new BorderPane();
+        DisplayPanel dp = getBean(DisplayPanel.class);
+        dp.getChildren().add(titledPane);
+        center.setCenter(dp);
+        center.setRight(right);
+        center.setTop(top);
+
+        return center;
     }
 
     protected Control buildLeft() {
 
-        List<TreeItem<TItem>> treeData = dataProvider2.getTreeCache();
+        CacheData dp = getBean(CacheData.class);
+        List<TreeItem<TItem>> treeData = dp.getTreeCache();
 
 //        TreeItem<String> childNode1 = new TreeItem<>("Node 1");
 //        TreeItem<String> childNode2 = new TreeItem<>("Node 2");
@@ -101,8 +149,9 @@ public class App extends Application {
 
         root.getChildren().addAll(treeData);
 
-        TreeTableColumn<TItem, String> column = new TreeTableColumn<>("Column");
-        column.setPrefWidth(150);
+        TreeTableColumn<TItem, String> column = new TreeTableColumn<>("Categories");
+        column.setPrefWidth(200);
+        column.setResizable(true);
 
         column.setCellValueFactory((TreeTableColumn.CellDataFeatures<TItem, String> p) -> new ReadOnlyStringWrapper(
                 p.getValue().getValue().getName()));
@@ -159,6 +208,24 @@ public class App extends Application {
         menuBar.getMenus().addAll(fileMenu, webMenu, sqlMenu);
 
         return menuBar;
+    }
+
+    public Set<Object> getAllBeans() {
+        return new HashSet<>(Arrays.asList(dataProvider, displayPanel));
+    }
+
+    public <T> T getBean(Class<T> type) {
+        App context = container.instance().select(App.class).get();
+        if (type == App.class)  {
+            return (T)context;
+        }
+
+        for (Object obj : context.getAllBeans()) {
+            if (obj.getClass() == type) {
+                return (T)obj;
+            }
+        }
+        return null;
     }
 }
 
